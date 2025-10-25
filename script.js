59213b450e95b85336b1acf1a99870d649ddf561
jaredmasters <mastersjared3@gmail.com>
2025-10-25 05:28:57 -0700

    complete

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
+const resizeButton = document.querySelector(".resize-button");
 
-for (let i = 0; i < 16; ++i) {
-    const rowContainer = document.createElement("div");
-    rowContainer.classList.add("row-container");
-    for (let j = 0; j < 16; ++j) {
-        const square = document.createElement("p");
-        square.classList.add("grid-square");
-        rowContainer.appendChild(square);
+function createGrid(size) {
+    for (let i = 0; i < size; ++i) {
+        const rowContainer = document.createElement("div");
+        rowContainer.classList.add("row-container");
+        for (let j = 0; j < size; ++j) {
+            const whiteSquare = document.createElement("p");
+            whiteSquare.classList.add("grid-square");
+            rowContainer.appendChild(whiteSquare);
+        }
+        grid.appendChild(rowContainer);
     }
-    container.appendChild(rowContainer);
 }
+
+grid.addEventListener("mouseover", e => {
+    if (e.target.classList.contains("grid-square")) {
+        if (e.target.style.backgroundColor == "") {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        }
+    }
+})
+
+resizeButton.addEventListener("click", () => {
+    const size = prompt("How many squares per side?");
+    if (size !== null && size.length > 0 && !isNaN(size)) {
+        Array.from(grid.children).forEach(row => row.parentNode.removeChild(row));
+        createGrid(size);
+    }
+})
+
+createGrid(16);
