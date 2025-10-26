8243877831401616ee1817066bdc0b2c3b094d4d
jaredmasters <mastersjared3@gmail.com>
2025-10-26 10:29:29 -0700

    add: eraser

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const grid = document.querySelector(".grid");
 
 const resizeButton = document.querySelector(".resize-button");
 const resetButton = document.querySelector(".reset-button");
+const eraserButton = document.querySelector(".eraser-button");
 const buttons = document.querySelectorAll(".button");
 
 let size = 16;
 let isMouseDown = false;
+let isEraserActive = false;
 
 function createGrid(size) {
     for (let i = 0; i < size; ++i) {
@@ -26,12 +28,15 @@ function clearGrid() {
 }
 
 function draw(e) {
-    if (e.target.classList.contains("grid-square") &&
-        e.target.style.backgroundColor == "" && isMouseDown) {
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
-        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    if (e.target.classList.contains("grid-square") && isMouseDown) {
+        if (isEraserActive) {
+            e.target.style.backgroundColor = "";
+        } else if (e.target.style.backgroundColor == "") {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        }
     }
 }
 
@@ -39,10 +44,11 @@ page.addEventListener("mousedown", e => {
     e.preventDefault();
     isMouseDown = true;
     draw(e);
-})
-page.addEventListener("mouseup", () => isMouseDown = false)
+});
 
-grid.addEventListener("mouseover", e => draw(e))
+page.addEventListener("mouseup", () => isMouseDown = false);
+
+grid.addEventListener("mouseover", e => draw(e));
 
 resizeButton.addEventListener("click", () => {
     let temp = size;
@@ -53,12 +59,22 @@ resizeButton.addEventListener("click", () => {
     } else {
         size = temp;
     }
-})
+});
 
 resetButton.addEventListener("click", () => {
     clearGrid();
     createGrid(size);
-})
+});
+
+eraserButton.addEventListener("click", () => {
+    if (isEraserActive) {
+        isEraserActive = false;
+        eraserButton.style.border = "2px solid black";
+    } else {
+        isEraserActive = true;
+        eraserButton.style.border = "2px solid red";
+    }
+});
 
 buttons.forEach(button => {
     button.addEventListener("mouseenter", e => {
@@ -72,6 +88,6 @@ buttons.forEach(button => {
             e.target.style.scale = 1.0;
         }
     })
-})
+});
 
 createGrid(size);
